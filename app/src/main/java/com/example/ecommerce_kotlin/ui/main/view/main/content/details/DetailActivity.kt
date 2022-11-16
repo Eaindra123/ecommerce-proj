d5d3ef040433e0206a51293cc5e01a03cfcb3718
Eaindra123 <eaindrae137@gmail.com>
2022-11-16 15:56:28 +0800

    add folder structure for ui

---
+++ app/src/main/java/com/example/ecommerce_kotlin/ui/main/view/main/content/details/DetailActivity.kt
@@ -0,0 +1,4 @@
+package com.example.ecommerce_kotlin.ui.main.view.main.content.details
+
+class DetailActivity {
+}
---
+++ app/src/main/java/com/example/ecommerce_kotlin/ui/main/view/main/content/listing/ListingActivity.kt
@@ -0,0 +1,4 @@
+package com.example.ecommerce_kotlin.ui.main.view.main.content.listing
+
+class ListingActivity {
+}
